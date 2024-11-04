bb9db2d8c7208f505e61435b0007650cf944a679
nurulsalmaafifah <23215013@mhs.stmik-tegal.ac.id>
2024-11-04 06:26:35 -0800

    Add files via upload

---
+++ latihan18-kelipatan.java
@@ -0,0 +1,10 @@
+run:
+Saldo di bulan ke-1 Rp. 2,875,000.00
+Saldo di bulan ke-2 Rp. 3,306,250.00
+Saldo di bulan ke-3 Rp. 3,802,187.50
+Saldo di bulan ke-4 Rp. 4,372,515.62
+Saldo di bulan ke-5 Rp. 5,028,392.97
+Saldo di bulan ke-6 Rp. 5,782,651.91
+Saldo di bulan ke-7 Rp. 6,650,049.70
+Saldo di bulan ke-8 Rp. 7,647,557.16
+BUILD SUCCESSFUL (total time: 0 seconds)
